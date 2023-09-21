bfc768cb33a97e85e0560e896dfff58a2270dafa
LoredanaHanganu <loredana999@gmail.com>
2023-09-21 18:20:39 +0200

    Resolved without method

--- src/unidad_1/Calculadora_Iva.java
+++ src/unidad_1/Calculadora_Iva.java
@@ -1,17 +1,21 @@
 package unidad_1;
+import java.util.Scanner;
 
 public class Calculadora_Iva {
     public static void main(String[] args) {
 
-        int baseImponible = 10;
-        double iva = 0.21;
+        Scanner sc = new Scanner(System.in);
 
-        double ivaProporcional = baseImponible * iva;
+        System.out.print("Introduzca la base imponible: ");
+        double baseImponible = sc.nextDouble();
+
+        System.out.print("Introduzca el IVA: ");
+        double porcentajeIva = sc.nextDouble();
+
+        double ivaProporcional = baseImponible * porcentajeIva/100;
         double precioTotal = baseImponible + ivaProporcional;
 
         System.out.println( "El IVA es: " + ivaProporcional);
         System.out.println("El precio total es: " + precioTotal);
-
-
     }
 }
